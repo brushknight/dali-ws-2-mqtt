6a9c5668c9577559dddf7ca1e0d15e1b56a54548
Grigorii Merkushev <brushknight@gmail.com>
2022-08-15 16:27:31 +0200

    fix startup scrip

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ MQTT_HOST=$(bashio::config 'mqtt_host')
 MQTT_PORT=$(bashio::config 'mqtt_port')
 MQTT_USER=$(bashio::config 'mqtt_user')
 MQTT_PASS=$(bashio::config 'mqtt_pass')
+DALI_2_IOT_HOST=$(bashio::config '  dali_2_iot_host')
 
-pip3 install -r requirements.txt
+ls -la
+
+#pip3 install -r requirements.txt
+pip3 install paho-mqtt websockets
 
 python3 main.py
